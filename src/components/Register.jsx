a1acdf757df65b5fe8f29695b2e8e08fa2f21934
vjuggler <vefaferzeliyeva11@gmail.com>
2024-01-17 16:33:16 +0400

    dynamically handle user credentials

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { auth } from '../firebase/config'
 
 export const Register = () => {
+  const [userCredentials, setUserCredentials] = useState({});
+
+  const handleCredentials = (e) => {
+    setUserCredentials({ ...userCredentials, [e.target.name]: e.target.value });
+    console.log(userCredentials);
+
+  }
 
   return (
     <>
       <form className="mt-[100px] max-w-sm mx-auto border-2 rounded-lg border-solid p-5">
         <h1 className='font-bold text-xl mb-5 text-center'>SIGN UP</h1>
+        <div className="mb-5">
+          <label
+            htmlFor="fullname"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Full Name
+          </label>
+          <input
+            type="text"
+            name="fullname"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Name"
+            required
+            onChange={(e) => handleCredentials(e)}
+          />
+        </div>
         <div className="mb-5">
           <label
             htmlFor="email"
@@ -15,10 +39,11 @@ export const Register = () => {
           </label>
           <input
             type="email"
-            id="email"
+            name="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="name@gmail.com"
             required
+            onChange={(e) => handleCredentials(e)}
           />
         </div>
         <div className="mb-5">
@@ -30,13 +55,14 @@ export const Register = () => {
           </label>
           <input
             type="password"
-            id="password"
+            name="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="********"
             required
+            onChange={(e) => handleCredentials(e)}
           />
         </div>
-      
+
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDbIzwsfzYSXMfK_NSN-VwBDxYjRLuABKE",
@@ -10,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
