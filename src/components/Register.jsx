6269822a1675a0abc2e0124b722be97554a6c2fe
vjuggler <vefaferzeliyeva11@gmail.com>
2024-01-17 15:22:21 +0400

    installed react-router-dom

---
+++ src/components/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Register = () => {
+    
+  return (
+    <>
+        <form>
+            <input type="text" placeholder='Email' />
+            <input type="password" placeholder='Password' />
+            <button type='submit'>Register</button>
+        </form>
+    </>
+  )
+}
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbIzwsfzYSXMfK_NSN-VwBDxYjRLuABKE",
+  authDomain: "fir-auth-6816a.firebaseapp.com",
+  projectId: "fir-auth-6816a",
+  storageBucket: "fir-auth-6816a.appspot.com",
+  messagingSenderId: "462969558784",
+  appId: "1:462969558784:web:6ace7f7f42c95e8d9105e5"
+};
+
+const app = initializeApp(firebaseConfig);
